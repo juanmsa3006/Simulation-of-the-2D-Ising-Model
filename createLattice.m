bd58024069ba178ae4a4c418f53c104cf3ce90ad
juanmsa3006 <juanmanuelsa3006@gmail.com>
2024-09-15 14:07:10 +0200

    createLattice.m

--- createLattice.m
+++ createLattice.m
@@ -1,9 +1,9 @@
 function L = createLattice(n1,n2,p)
 % L = createLattice(n1,n2,p)
-% n1 : escalar, numero de filas                                                [INPUT]
-% n2 : escalar, numero de columnas                                             [INPUT]
-% p  : escalar en el rango (0,1), probabilidad de que cualquier entrada sea -1 [INPUT]
-% L  : lattice, matriz n1xn2 con entradas 1 y -1                               [OUTPUT]
+% n1 : scalar, number of rows                                                [INPUT]
+% n2 : scalar, number of columns                                             [INPUT]
+% p  : scalar in the (0,1) range, probability that any cell is set to -1     [INPUT]
+% L  : lattice, matrix n1xn2 with entries 1 and -1                           [OUTPUT]
 
 L = rand(n1,n2);
 for i = 1:n1*n2
