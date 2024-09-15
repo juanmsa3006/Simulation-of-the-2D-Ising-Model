8cb4f80dcc1c356b3eaac73f5dc356a0cdbb1fb4
juanmsa3006 <juanmanuelsa3006@gmail.com>
2024-09-15 14:13:55 +0200

    extraHamil.m

--- extraHamil.m
+++ extraHamil.m
@@ -1,7 +1,7 @@
 function E = extraHamil(lattice)
 % E = extraHamil(lattice)
-% lattice : lattice cuya energia queremos calcular [INPUT]
-% E       : escalar, energia del lattice           [OUTPUT]
+% lattice : lattice whose energy we want to calculate [INPUT]
+% E       : scalar, energy of the lattice             [OUTPUT]
 
 [n1,n2] = size(lattice);
 sum = 0;
@@ -11,7 +11,7 @@ for i = 1:n1
 	for j = 1:n2
 		for k = 1:n1
 			for l = 1:n2
-				if i~=k || j ~= l % si no son el mismo elemento del lattice
+				if i~=k || j ~= l % if they are not the same element
 					sum = sum+lattice(i,j)*lattice(k,l)/(sqrt(dot([i,j]-[k,l],[i,j]-[k,l])));
 				end
 			end			
