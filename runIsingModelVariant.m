95ad96ed633472c995d437e57db9f28aaf7063f8
juanmsa3006 <juanmanuelsa3006@gmail.com>
2024-09-15 14:26:49 +0200

    runIsingModelVariant.m

--- runIsingModelVariant.m
+++ runIsingModelVariant.m
@@ -1,14 +1,14 @@
 function [lattice_mu,avg_spin,tot_energy, eff] = runIsingModelVariant(lattice_mu,t, c, graph)
 % [lattice_mu,avg_spin,tot_energy,eff] = runIsingModel(lattice_mu,t, c, h, graph)
-% lattice_mu : lattice incial                                                            [INPUT]
-% t          : numero de iteraciones realizadas                                          [INPUT]
-% c          : valor de la constante beta                                                [INPUT]
-% h          : matriz del mismo tamano que el lattice con los valores de H en cada punto [INPUT]
-% graph      : boolean, si es verdadero grafica los resultado                            [INPUT]
-% lattice_mu : lattice final                                                             [OUTPUT]
-% avg_spin   : vector con la magnetizacion del lattice en cada paso temporal             [OUTPUT]
-% tot_energy : vector con la energia total del lattice en cada paso temporal             [OUTPUT]
-% eff        : escalar, numero de alteraciones de espin sobre pasos temporales           [OUTPUT]
+% lattice_mu : initial lattice                                                                      [INPUT]
+% t          : number of iterations                                                                 [INPUT]
+% c          : value of the constant beta                                                           [INPUT]
+% h          : matrix of the same dimensions as the lattice with the H-field value at each point    [INPUT]
+% graph      : boolean, if true plots the results                                                   [INPUT]
+% lattice_mu : final lattice                                                                        [OUTPUT]
+% avg_spin   : vector storing the magnetization of the lattice at each time step                    [OUTPUT]
+% tot_energy : vector storing the energy of the lattice at each time step                           [OUTPUT]
+% eff        : scalar, number of spins flipped per number of time steps                             [OUTPUT]
 
 Loriginal = lattice_mu;
 
@@ -17,11 +17,11 @@ avg_spin = zeros(1,t);
 tot_energy = zeros(1,t);
 
 for i = 1:t
-    r = 1 + floor(numel(lattice_mu)*rand); % seleccionar paticula a cambiar spin
+    r = 1 + floor(numel(lattice_mu)*rand); % selects spin to be flipped
     lattice_v = lattice_mu;
     lattice_v(r) = -lattice_v(r);
     dE = extraHamil(lattice_v)-extraHamil(lattice_mu);
-    if dE < 0 % estado v tiene menor energia que mu
+    if dE < 0 % state v has less energy than state mu
         lattice_mu = lattice_v;
     end
     if dE > 0
@@ -48,14 +48,14 @@ if graph
     figure(1)
     plot(1:t,tot_energy)
     title('E vs. t')
-    xlabel('tiempo')
+    xlabel('time')
     ylabel('E(J)')
     
     figure(2)
     plot(1:t,avg_spin)
-    title('Magnetizacion vs. tiempo')
-    xlabel('tiempo')
-    ylabel('Magnetizacion total')
+    title('Magnetization vs. time')
+    xlabel('time')
+    ylabel('Total Magnetization')
     
     figure(3)
     imagesc(Loriginal)
@@ -63,7 +63,7 @@ if graph
     
     figure(4)
     imagesc(lattice_mu)
-    title('Lattice Final')
+    title('Final Lattice')
 end
 
 end
