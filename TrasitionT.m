7b3e92c9350098fba3f399754e7c146ca9cf4eba
juanmsa3006 <juanmanuelsa3006@gmail.com>
2024-09-15 14:04:10 +0200

    TrasitionT.m

--- TrasitionT.m
+++ TrasitionT.m
@@ -1,15 +1,15 @@
 clear, clc, close all
 kBT = [linspace(0.01,2,4), linspace(2.001, 2.4, 6), linspace(2.45,4, 6),linspace(4.001,10, 4)];
 t = 10^5;
-all_spin  =zeros(1,numel(kBT)); % vector que almancenera los resultados de la magnetizacion de cada simulacion 
+all_spin  =zeros(1,numel(kBT)); % vector stores magnetization results for each simulation  
 H  = zeros(20,20);
-% Bucle que realiza una simulacion para cada valor de kBT
+% Loop which runs the simulation for each value of kBT
 for i = 1:numel(kBT)
     beta = 1./(kBT(i));
     lattice_mu = createLattice(20,20,0);
     [lattice_final,avg_spin,tot_energy] = runIsingModel(lattice_mu,t,beta,H, false);
     all_spin(i) = (sum(avg_spin(end-49:end))./50);
-    disp(['Simulacion numero: ', num2str(i), '/20'])
+    disp(['Run number: ', num2str(i), '/20'])
 end
 
 all_spin_neg  =zeros(1,numel(kBT));
@@ -19,11 +19,11 @@ for i = 1:numel(kBT)
     lattice_mu = createLattice(20,20,1);
     [lattice_final,avg_spin,tot_energy] = runIsingModel(lattice_mu,t,beta,H, false);
     all_spin_neg(i) = (sum(avg_spin(end-49:end))./50);
-    disp(['Simulacion numero: ', num2str(i), '/20'])
+    disp(['Run number: ', num2str(i), '/20'])
 end
 
 
-%% Grafica
+%% Plot
 
 figure(1)
 plot(kBT, all_spin, 'g o','markerfacecolor', 'g')
@@ -35,6 +35,6 @@ xline(2.269, 'r')
 grid on
 xlabel('k_BT (J)')
 ylabel('M')
-title('Temperatura de transicion de estado')
+title('Phase Transition Temperature')
 
-legend('Positivos', 'Negativos', 'k_BT_c = 2.269 J','Location','northeast')
+legend('Positive', 'Negative', 'k_BT_c = 2.269 J','Location','northeast')
